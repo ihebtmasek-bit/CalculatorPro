405b42fe91383505e587e739b04a9432a8a2444c
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-24 23:16:36 +0100

    Use ultra-simple valid project structure

--- app/src/main/java/com/calculatorpro/app/MainActivity.java
+++ app/src/main/java/com/calculatorpro/app/MainActivity.java
@@ -1,31 +1,18 @@
 package com.calculatorpro.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebSettings;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         WebView webView = new WebView(this);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAllowFileAccess(true);
-        
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-        
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/calculator.html");
+        
         setContentView(webView);
     }
 }
